93d6751dad5297a14f25c0a46fdc3e14d39cfb07
rkcoden <khanra4@farmingdale.edu>
2023-01-30 23:39:34 +0000

    done

--- src/main/java/ReturnArrayContents.java
+++ src/main/java/ReturnArrayContents.java
@@ -28,6 +28,11 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < nums.length; i++)
+            sb.append(nums[i]).append(" ");
+        
+        return sb.toString().trim();
     }
 }
